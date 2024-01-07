d856e30654227f15d3e5620ebaccdfc3daf62a57
JahanavDixit <jahanavdixit.20.ce@iite.indusuni.ac.in>
2024-01-08 03:32:23 +0530

    conflicts

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_openai import ChatOpenAI
-from llama_index import Document
 import os
 import tempfile
 
